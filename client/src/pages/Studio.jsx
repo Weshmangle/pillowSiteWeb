e6c6b09e7fe816308aea7abb2367d2c3af9c1370
Amadinho94 <amadinho94@icloud.com>
2024-05-13 15:59:28 +0000

    Page Studio v1

--- client/src/pages/Studio.jsx
+++ client/src/pages/Studio.jsx
@@ -3,6 +3,37 @@ import { useEffect } from 'react'
 
 const Studio = () => {
     
+    const paragArray = [
+            {
+               id: 0,
+               title : "Lorem ipsum",
+               paragraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ornare quam eget justo luctus, sed convallis sapien tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in eleifend lacus. Sed ut leo magna. Maecenas consectetur mauris et neque condimentum, nec venenatis odio faucibus. Curabitur sed est eget justo vehicula tempor nec a mauris. Proin suscipit auctor urna, nec commodo mi lobortis id. Integer suscipit, nisi vitae dapibus hendrerit, dui quam congue purus, vitae aliquam elit ligula sit amet ligula. Vivamus at velit in ligula interdum ullamcorper. Etiam tempus, ligula sit amet venenatis volutpat, mauris sapien bibendum urna, id malesuada ligula magna sed lectus. Suspendisse potenti. Nunc auctor feugiat justo, vitae tempor elit efficitur et. Vivamus tempor velit vel libero vehicula, nec elementum libero convallis. Curabitur vel risus vel turpis pharetra faucibus eget ut mauris."
+            },
+            
+            {
+               id: 1,
+               title : "Lorem ipsum",
+               paragraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ornare quam eget justo luctus, sed convallis sapien tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in eleifend lacus. Sed ut leo magna. Maecenas consectetur mauris et neque condimentum, nec venenatis odio faucibus. Curabitur sed est eget justo vehicula tempor nec a mauris. Proin suscipit auctor urna, nec commodo mi lobortis id. Integer suscipit, nisi vitae dapibus hendrerit, dui quam congue purus, vitae aliquam elit ligula sit amet ligula. Vivamus at velit in ligula interdum ullamcorper. Etiam tempus, ligula sit amet venenatis volutpat, mauris sapien bibendum urna, id malesuada ligula magna sed lectus. Suspendisse potenti. Nunc auctor feugiat justo, vitae tempor elit efficitur et. Vivamus tempor velit vel libero vehicula, nec elementum libero convallis. Curabitur vel risus vel turpis pharetra faucibus eget ut mauris."
+            },
+            
+            {
+               id: 2,
+               title : "Lorem ipsum",
+               paragraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ornare quam eget justo luctus, sed convallis sapien tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in eleifend lacus. Sed ut leo magna. Maecenas consectetur mauris et neque condimentum, nec venenatis odio faucibus. Curabitur sed est eget justo vehicula tempor nec a mauris. Proin suscipit auctor urna, nec commodo mi lobortis id. Integer suscipit, nisi vitae dapibus hendrerit, dui quam congue purus, vitae aliquam elit ligula sit amet ligula. Vivamus at velit in ligula interdum ullamcorper. Etiam tempus, ligula sit amet venenatis volutpat, mauris sapien bibendum urna, id malesuada ligula magna sed lectus. Suspendisse potenti. Nunc auctor feugiat justo, vitae tempor elit efficitur et. Vivamus tempor velit vel libero vehicula, nec elementum libero convallis. Curabitur vel risus vel turpis pharetra faucibus eget ut mauris."
+            },
+            
+            {
+               id: 3,
+               title : "Lorem ipsum",
+               paragraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ornare quam eget justo luctus, sed convallis sapien tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in eleifend lacus. Sed ut leo magna. Maecenas consectetur mauris et neque condimentum, nec venenatis odio faucibus. Curabitur sed est eget justo vehicula tempor nec a mauris. Proin suscipit auctor urna, nec commodo mi lobortis id. Integer suscipit, nisi vitae dapibus hendrerit, dui quam congue purus, vitae aliquam elit ligula sit amet ligula. Vivamus at velit in ligula interdum ullamcorper. Etiam tempus, ligula sit amet venenatis volutpat, mauris sapien bibendum urna, id malesuada ligula magna sed lectus. Suspendisse potenti. Nunc auctor feugiat justo, vitae tempor elit efficitur et. Vivamus tempor velit vel libero vehicula, nec elementum libero convallis. Curabitur vel risus vel turpis pharetra faucibus eget ut mauris."
+            },
+            
+            {
+               id: 4,
+               title : "Lorem ipsum",
+               paragraph : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ornare quam eget justo luctus, sed convallis sapien tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in eleifend lacus. Sed ut leo magna. Maecenas consectetur mauris et neque condimentum, nec venenatis odio faucibus. Curabitur sed est eget justo vehicula tempor nec a mauris. Proin suscipit auctor urna, nec commodo mi lobortis id. Integer suscipit, nisi vitae dapibus hendrerit, dui quam congue purus, vitae aliquam elit ligula sit amet ligula. Vivamus at velit in ligula interdum ullamcorper. Etiam tempus, ligula sit amet venenatis volutpat, mauris sapien bibendum urna, id malesuada ligula magna sed lectus. Suspendisse potenti. Nunc auctor feugiat justo, vitae tempor elit efficitur et. Vivamus tempor velit vel libero vehicula, nec elementum libero convallis. Curabitur vel risus vel turpis pharetra faucibus eget ut mauris."
+            }
+        ]
     
     useEffect(() => {
         
@@ -14,10 +45,21 @@ const Studio = () => {
     
     return (
         <main className="studio-page-main container">
+            <h1 className="studio-page-title">Notre studio</h1>
             
-            {/****** Article **********/}
+            {/****** Contenu de la page **********/}
             <article className="studio-page-article">
-                <p>Lorem ipsum</p>
+                {paragArray.map((oneParag, index) => (
+                    
+                    <>
+                    {/**** Section avec un paragraphe ****/}
+                    <section key={oneParag.id} className="studio-page-section">
+                        <h2>{oneParag.title}</h2>
+                        <p className="studio-page-paragraph">{oneParag.paragraph}</p>
+                    </section>
+                    </>
+                    
+                ))}
             </article>
             
         </main>
