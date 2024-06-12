67e51ef4bc3fcfa0f15ebaf9fa987e66f87de48d
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2024-06-12 15:54:45 +0200

    Update actions

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -2,9 +2,9 @@ cd client
 ls
 pwd
 echo VITE_API_URL = {
-echo ${{ vars.URL_FRONT }}
+echo $URL_FRONT
 echo }
-echo VITE_API_URL = ${{ vars.URL_FRONT }} > .env
+echo VITE_API_URL = $URL_FRONT > .env
 npm install
 npm run build
 chmod +x ./../build-scripts/copy_build_to_server.sh && ./../build-scripts/copy_build_to_server.sh
