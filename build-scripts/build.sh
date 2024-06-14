9929703bcdeaced2da952a3fb64a39979186ec10
Weshmangle <96111319+Weshmangle@users.noreply.github.com>
2024-06-14 15:31:24 +0200

    Update build.sh

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,7 +1,14 @@
 ls
+
+echo $SSH_KEY > ~/.ssh/id_ed25519.pub
+ls ~/.ssh/id_ed25519.pub
+echo "file" > lolilol
+scp lolilol weshmangle_ssh@ssh-weshmangle.alwaysdata.net:~/
+
 cd client
 pwd
 echo VITE_API_URL = $URL_FRONT > .env
+
 npm install
 npm run build
 chmod +x ./../build-scripts/copy_build_to_server.sh && ./../build-scripts/copy_build_to_server.sh
